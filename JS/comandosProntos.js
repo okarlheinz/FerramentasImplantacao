7885673abdbd5182c4f32d8617699597357670e4
Karl Heinz <110443535+okarlheinz@users.noreply.github.com>
2024-07-05 11:39:40 -0300

    melhorias no codigo comandosProntos.js

--- JS/comandosProntos.js
+++ JS/comandosProntos.js
@@ -27,18 +27,4 @@ $(document).ready(function () {
     $('.alert button.close').click(function () {
         $('.alert').fadeOut();
     });
-
-    // Função para gerar o comando com base no exemplo selecionado
-    function gerarComando(exemplo) {
-        switch (exemplo) {
-            case "GradeProdCorrecao":
-                return "INSERT INTO gradeProd (codigoBase,codigoCor,codigoTamanho,codigoGrade,codigoBarra,filiaisPendentes,descricaoGrade,gradeVendaPrc,gradeQtdMinima,gradeAtivo,GRADEDATAINCLUSAO) SELECT codigo, '0', '0', codigo, codigoBarraProduto, 0, descricao, vendaprc, 0, '1', DATAINCLUSAO FROM produto WHERE codigo NOT IN (SELECT codigograde FROM gradeprod)";
-            case "FilialEstoqueCorrecao":
-                return "insert into filialEstoque (prodCodigo,idFilial,qtdDisp,qtdEst,qtdReserv,qtdAssist,qtdPed, entradas,saidas,filiaisPendentes,qtddispbal,qtdconsignacao,qtddev,qtdinicio) select codigograde,?,0,0,0,0,0,0,0,0,0,0,0,0 from gradeprod where codigograde not in (select prodCodigo from filialEstoque where idfilial=?)";
-            case "FiliaisPendentes":
-                return "update TABELA set filiaispendentes=filiaispendentes | (select (power(2,(select max(idfilial)-1 from filial))*2)-2)"
-            default:
-                return "";
-        }
-    }
 });
