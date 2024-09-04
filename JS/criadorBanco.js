61f3ad5a4176a6bf42fe61453cf4fbdc958eae76
Karl Heinz <110443535+okarlheinz@users.noreply.github.com>
2024-09-04 14:39:00 -0300

    Melhorias na tela de criação de banco, inserindo regime e servidor

--- JS/criadorBanco.js
+++ JS/criadorBanco.js
@@ -1,37 +1,98 @@
 function generateCommand() {
-    const idempresa = document.getElementById('idempresa').value;
-    const nomefantasia = document.getElementById('nomefantasia').value;
-    const razaosocial = document.getElementById('razaosocial').value;
-    const cnpj = document.getElementById('cnpj').value;
-    const inscest = document.getElementById('inscest').value;
-    const cep = document.getElementById('cep').value;
-    const logradouro = document.getElementById('logradouro').value;
-    const numero = document.getElementById('numero').value;
-    const bairro = document.getElementById('bairro').value;
-    const complemento = document.getElementById('complemento').value;
-    const cidade = document.getElementById('cidade').value;
-    const estado = document.getElementById('estado').value;
-    const nomegeral = document.getElementById('nomegeral').value;
-    const fone = document.getElementById('fone').value;
-    const celular = document.getElementById('celular').value;
-    const email = document.getElementById('email').value;
-    const caixaloja = document.getElementById('caixaloja').value;
-    const empresavalidacao = document.getElementById('empresavalidacao').value;
-    const senhavalidacao = document.getElementById('senhavalidacao').value;
+    const idempresa = document.getElementById('idempresa').value.toUpperCase();
+    const nomefantasia = document.getElementById('nomefantasia').value.toUpperCase();
+    const razaosocial = document.getElementById('razaosocial').value.toUpperCase();
+    const cnpj = document.getElementById('cnpj').value.toUpperCase();
+    const inscest = document.getElementById('inscest').value.toUpperCase();
+    const cep = document.getElementById('cep').value.toUpperCase();
+    const logradouro = document.getElementById('logradouro').value.toUpperCase();
+    const numero = document.getElementById('numero').value.toUpperCase();
+    const bairro = document.getElementById('bairro').value.toUpperCase();
+    const complemento = document.getElementById('complemento').value.toUpperCase();
+    const cidade = document.getElementById('cidade').value.toUpperCase();
+    const estado = document.getElementById('estado').value.toUpperCase();
+    const nomegeral = document.getElementById('nomegeral').value.toUpperCase();
+    const fone = document.getElementById('fone').value.toUpperCase();
+    const celular = document.getElementById('celular').value.toUpperCase();
+    const email = document.getElementById('email').value.toUpperCase();
+    const caixaloja = document.getElementById('caixaloja').value.toUpperCase();
+    const empresavalidacao = document.getElementById('empresavalidacao').value.toUpperCase();
+    const senhavalidacao = document.getElementById('senhavalidacao').value.toUpperCase();
+    const servidor = document.getElementById('servidor').checked;
+    const regime = document.getElementById('regime').value;
 
     if (!idempresa || !nomefantasia || !razaosocial || !cnpj) {
         alert("Por favor, preencha todos os campos obrigatórios.");
         return;
     }
 
+    // Dicionário de estados
+    const estados = {
+        'AC': 'ACRE',
+        'AL': 'ALAGOAS',
+        'AP': 'AMAPÁ',
+        'AM': 'AMAZONAS',
+        'BA': 'BAHIA',
+        'CE': 'CEARÁ',
+        'DF': 'DISTRITO FEDERAL',
+        'ES': 'ESPÍRITO SANTO',
+        'GO': 'GOIÁS',
+        'MA': 'MARANHÃO',
+        'MT': 'MATO GROSSO',
+        'MS': 'MATO GROSSO DO SUL',
+        'MG': 'MINAS GERAIS',
+        'PA': 'PARÁ',
+        'PB': 'PARAÍBA',
+        'PR': 'PARANÁ',
+        'PE': 'PERNAMBUCO',
+        'PI': 'PIAUÍ',
+        'RJ': 'RIO DE JANEIRO',
+        'RN': 'RIO GRANDE DO NORTE',
+        'RS': 'RIO GRANDE DO SUL',
+        'RO': 'RONDÔNIA',
+        'RR': 'RORAIMA',
+        'SC': 'SANTA CATARINA',
+        'SP': 'SÃO PAULO',
+        'SE': 'SERGIPE',
+        'TO': 'TOCANTINS'
+    };
+
+    const regional = estados[estado] || estado;
+
+    // Determina o valor de CRT
+    let crt = '';
+    if (regime === 'simples') {
+        crt = '1';
+    } else if (regime === 'normal') {
+        crt = '3';
+    }
+
     const insertEmpresa = `
-INSERT INTO empresa (idempresa, nomefantasia, razaosocial, cnpj, inscest, cep, logradouro, numero, bairro, complemento, cidade, estado, fone, fax, email) 
-VALUES (${idempresa}, \r\n'${nomefantasia}',\r\n '${razaosocial}',\r\n '${cnpj}',\r\n '${inscest}',\r\n '${cep}',\r\n '${logradouro}',\r\n '${numero}',\r\n '${bairro}',\r\n '${complemento}',\r\n '${cidade}',\r\n '${estado}',\r\n '${fone}',\r\n '${celular}',\r\n '${email}');
-    `;
+INSERT INTO empresa (idempresa, nomefantasia, razaosocial, cnpj, inscest, cep, logradouro, numero, bairro, complemento, cidade, estado, fone, fax, email${crt ? ', crt' : ''}) 
+VALUES 
+(${idempresa},
+'${nomefantasia}',
+'${razaosocial}',
+'${cnpj}',
+'${inscest}',
+'${cep}',
+'${logradouro}',
+'${numero}',
+'${bairro}',
+'${complemento}',
+'${cidade}',
+'${estado}',
+'${fone}',
+'${celular}',
+'${email}',
+${crt ? `'${crt}'` : ''});`;
 
     const insertFilial = `
-INSERT INTO filial (idfilial, nomefantasia, razaosocial, cnpj, inscest, cep, logradouro, numero, bairro, complemento, cidade, estado, nomegeral, fone, fax, email, caixaloja) 
+INSERT INTO filial (idfilial, status, regional, subregional, nomefantasia, razaosocial, cnpj, inscest, cep, logradouro, numero, bairro, complemento, cidade, estado, nomegeral, fone, fax, email, caixaloja) 
 VALUES (${idempresa},
+'1',
+'${regional}',
+'${cidade}',
 '${nomefantasia}',
 '${razaosocial}',
 '${cnpj}',
@@ -61,13 +122,17 @@ WHERE IDFILIAL = ${idempresa};
 INSERT INTO EMPRESAVALIDACAO (FLAG, USUARIO, CHAVE) VALUES ('1','${empresavalidacao}','${senhavalidacao}')
 `;
 
-    const output = insertEmpresa + "\ngo\n" + insertFilial + "\ngo\n" + insertCaixa + "\ngo\n" + insertEmpresaValidacao;
+    const updateServidor = `
+UPDATE configuracao SET servidor='${servidor ? '1' : '0'}';
+`;
+
+    const output = insertEmpresa + "\ngo\n" + insertFilial + "\ngo\n" + insertCaixa + "\ngo\n" + insertEmpresaValidacao + "\ngo\n" + updateServidor;
 
     document.getElementById('commandOutput').value = output.trim();
     document.getElementById('copyCommandButton').style.display = 'inline';
     document.getElementById('insertFilialButton').style.display = 'inline';
-
 }
+
 
 function copyCommandToClipboard() {
     const outputText = document.getElementById('commandOutput').value;
@@ -95,7 +160,7 @@ $(document).ready(function() {
     $('#cnpj').inputmask("99.999.999/9999-99");
 
     // Limpeza dos campos para apenas números
-    $('#fone, #celular, #inscest, #cep').on('input', function() {
+    $('#fone, #celular, #cep').on('input', function() {
         var valor = $(this).val().replace(/\D/g, '');
         $(this).val(valor);
     });
