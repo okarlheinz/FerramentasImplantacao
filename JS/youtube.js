dbbf0f2f756086b9509096e549c593c59ef33fb7
Karl Heinz <110443535+okarlheinz@users.noreply.github.com>
2024-09-16 12:10:32 -0300

    adicionado campo de busca na tela de videos do youtube

--- JS/youtube.js
+++ JS/youtube.js
@@ -1,34 +1,49 @@
 $(document).ready(function() {
+    // Carregar vídeos a partir do JSON
     $.getJSON("/JSON/videos.json", function(data) {
         let videosContainer = $("#videos-container");
-        data.forEach(function(video) {
-            // Extrair o ID do vídeo do URL
-            let videoId = video.url.split('v=')[1] || video.url.split('/').pop();
-            let videoCard = `
-                <div class="col-md-4 mb-4">
-                    <div class="card video-card" data-video-id="${videoId}" data-video-title="${video.title}">
-                        <iframe class="card-img-top" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        <div class="card-body">
-                            <h5 class="card-title">${video.title}</h5>
+        
+        // Função para renderizar os vídeos
+        function renderVideos(videos) {
+            videosContainer.empty();
+            videos.forEach(function(video) {
+                let videoId = video.url.split('v=')[1] || video.url.split('/').pop();
+                let videoCard = `
+                    <div class="col-md-4 mb-4 video-card" data-title="${video.title.toLowerCase()}">
+                        <div class="card" data-video-id="${videoId}" data-video-title="${video.title}">
+                            <iframe class="card-img-top" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+                            <div class="card-body">
+                                <h5 class="card-title">${video.title}</h5>
+                            </div>
                         </div>
                     </div>
-                </div>
-            `;
-            videosContainer.append(videoCard);
-        });
+                `;
+                videosContainer.append(videoCard);
+            });
 
-        // Adicionar evento de clique aos cards
-        $('.video-card').on('click', function() {
-            let videoId = $(this).data('video-id');
-            let videoTitle = $(this).data('video-title');
-            $('#videoModalLabel').text(videoTitle);
-            $('#videoFrame').attr('src', `https://www.youtube.com/embed/${videoId}`);
-            $('#videoModal').modal('show');
-        });
+            // Adicionar evento de clique aos vídeos
+            $('.video-card').on('click', function() {
+                let videoId = $(this).find('.card').data('video-id');
+                let videoTitle = $(this).find('.card').data('video-title');
+                $('#videoModalLabel').text(videoTitle);
+                $('#videoFrame').attr('src', `https://www.youtube.com/embed/${videoId}`);
+                $('#videoModal').modal('show');
+            });
 
-        // Limpar o src do iframe quando o modal é fechado
-        $('#videoModal').on('hidden.bs.modal', function () {
-            $('#videoFrame').attr('src', '');
+            // Limpar o src do iframe quando o modal é fechado
+            $('#videoModal').on('hidden.bs.modal', function () {
+                $('#videoFrame').attr('src', '');
+            });
+        }
+
+        // Inicialmente renderizar todos os vídeos
+        renderVideos(data);
+
+        // Função para filtrar os vídeos com base na busca
+        $('#searchField').on('keyup', function () {
+            var searchTerm = $(this).val().toLowerCase();
+            let filteredVideos = data.filter(video => video.title.toLowerCase().includes(searchTerm));
+            renderVideos(filteredVideos);
         });
     });
 });
